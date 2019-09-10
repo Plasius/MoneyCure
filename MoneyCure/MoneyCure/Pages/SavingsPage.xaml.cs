e48e19b208998dc240c6850edb201244bcc93749
reka340 <reka340@gmail.com>
2019-09-10 18:56:14 +0300

    AddingSavingPage

--- MoneyCure/MoneyCure/Pages/SavingsPage.xaml.cs
+++ MoneyCure/MoneyCure/Pages/SavingsPage.xaml.cs
@@ -16,5 +16,46 @@ namespace MoneyCure.Pages
         {
             InitializeComponent();
         }
+        async void Editbt(object sender, EventArgs args)
+        {
+            
+        }
+        public async void Addbt(object sender, EventArgs args)
+        {
+
+        }
+        async void More(object sender,EventArgs args)
+        {
+            
+        }
+        async void Bt1_(object sender, EventArgs args)
+        {
+            Bt1.BackgroundColor = Color.LightSkyBlue;
+            Bt2.BackgroundColor = Color.LightGray;
+            Bt3.BackgroundColor = Color.LightGray;
+            Bt4.BackgroundColor = Color.LightGray;
+            
+        }
+        async void Bt2_(object sender, EventArgs args)
+        {
+            Bt1.BackgroundColor = Color.LightGray;
+            Bt2.BackgroundColor = Color.LightSkyBlue;
+            Bt3.BackgroundColor = Color.LightGray;
+            Bt4.BackgroundColor = Color.LightGray;
+        }
+        async void Bt3_(object sender, EventArgs args)
+        {
+            Bt1.BackgroundColor = Color.LightGray;
+            Bt2.BackgroundColor = Color.LightGray;
+            Bt3.BackgroundColor = Color.LightSkyBlue;
+            Bt4.BackgroundColor = Color.LightGray;
+        }
+        async void Bt4_(object sender, EventArgs args)
+        {
+            Bt1.BackgroundColor = Color.LightGray;
+            Bt2.BackgroundColor = Color.LightGray;
+            Bt3.BackgroundColor = Color.LightGray;
+            Bt4.BackgroundColor = Color.LightSkyBlue;
+        }
     }
 }
