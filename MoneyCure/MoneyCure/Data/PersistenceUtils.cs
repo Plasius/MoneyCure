b446f80e4b808625c257fbfcec87659034f7d4d5
Plasius <Plasius@users.noreply.github.com>
2019-09-11 16:58:47 +0100

    Create PersistenceUtils.cs

---
+++ MoneyCure/MoneyCure/Data/PersistenceUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyCure.Data
+{
+    class PersistenceUtils
+    {
+        public enum Categories
+        {
+            Income = -1,
+            Savings = 0,
+            Other = 1,
+            Personal = 2,
+            Food = 3,
+            Social = 4,
+            Education = 5,
+            Transport = 6,
+            Household = 7,
+            Apparel = 8,
+            Health = 9
+        }
+
+        public int PINCode { get; set; }
+        public int SavingsGoal { get; set; }
+
+    }
+}
