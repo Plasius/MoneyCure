f0d820e52124864fcd8783b608628d0e44ed28cc
reka340 <reka340@gmail.com>
2019-09-16 17:15:02 +0300

    huhu

--- MoneyCure/MoneyCure/Pages/SettingsPage.xaml.cs
+++ MoneyCure/MoneyCure/Pages/SettingsPage.xaml.cs
@@ -20,12 +20,16 @@ namespace MoneyCure.Pages
 
         async void Subbt (object sender, EventArgs args)
         {
+            Data.Utils.GetInstance().SetString()
             if (Pin1.Text != Pin2.Text)
             {
                 DisplayAlert("Error","Pin doesn't match","Try again");
             }
             else
             {
+                if (Pin2 == null)
+                    DisplayAlert("Error", "You must set a PIN", "OK");
+                Data.Utils.GetInstance().SetInt("PINCode", -1);
                 Navigation.PopAsync();
             }
         }
