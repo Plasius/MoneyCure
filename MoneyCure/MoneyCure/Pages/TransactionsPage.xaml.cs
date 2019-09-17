85e940efe0cdf591b2a5d7cfb9ad176eae10489d
Plasius <Plasius@users.noreply.github.com>
2019-09-17 15:45:39 +0100

    Finishing TransactionsPage

--- MoneyCure/MoneyCure/Pages/TransactionsPage.xaml.cs
+++ MoneyCure/MoneyCure/Pages/TransactionsPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -59,5 +60,17 @@ namespace MoneyCure.Pages
 
 
 
+    }
+
+    public class CategoryConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Data.Utils.Categories)((int)value)).ToString();
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
     }
 }
